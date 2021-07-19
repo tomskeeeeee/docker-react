39215fb61b5c2458d74c3f3df940ed427e9f998c
Sharon Tomski <hokiebadger@gmail.com>
2021-07-19 11:06:33 -0500

    added England Dan retesting AWS

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Bucks in 6.
+          Edit <code>src/App.js</code> and England Dan.
         </p>
         <a
           className="App-link"
