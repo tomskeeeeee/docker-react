aa09fbfb41003bf3842e12795c058e28fe55ef69
Sharon Tomski <hokiebadger@gmail.com>
2021-07-19 09:50:40 -0500

    resaved dockerfile bucks in 6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and feel the force.
+          Edit <code>src/App.js</code> and Bucks in 6.
         </p>
         <a
           className="App-link"
